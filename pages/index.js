efa052e46850fbf11a338cbb1f2f2a94d4081054
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-11-19 13:52:31 -0700

    routing

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import Link from "next/link";
 
 export default function Home() {
-  <div style={{ height: "100vh" }}>
-    <Stack
-      spacing={2}
-      justifyContent="center"
-      alignItems="center"
-      style={{ height: "100vh" }}
-    >
-      <Typography variant="h1" component="div" gutterBottom>
-        What do you want to Upload?
-      </Typography>
-      <Stack direction="row" spacing={2}>
-        <Button variant="outlined">Quiz</Button>
-        <Button variant="outlined">Lesson</Button>
+  return (
+    <div style={{ height: "100vh" }}>
+      <Stack
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
+        <Typography variant="h1" component="div" gutterBottom>
+          What do you want to Upload?
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined">Quiz</Button>
+          <Link href="lesson">
+            <Button variant="outlined">Lesson</Button>
+          </Link>
+        </Stack>
       </Stack>
-    </Stack>
-  </div>;
+    </div>
+  );
 }
