26bb5c7fb66cb6e7864cac6a1cb8289aba3ea0e7
Sooryun Kim <ksr9405@gmail.com>
2022-11-19 14:16:24 -0700

    lesson

--- pages/lesson.js
+++ pages/lesson.js
@@ -1,8 +1,12 @@
 import {
   Box,
   Button,
+  FormControl,
   FormLabel,
   Input,
+  InputLabel,
+  MenuItem,
+  Select,
   TextField,
   Typography,
 } from '@mui/material';
@@ -12,19 +16,46 @@ import React from 'react';
 function lesson() {
   return (
     <div>
-      <Typography>Lesson</Typography>
-      <Container>
+      <Typography variant='h3' sx={{ textAlign: 'center' }}>
+        Lesson
+      </Typography>
+      <Container sx={{ marginTop: '3rem' }}>
         <form>
-          <FormLabel>Upload Image</FormLabel>
-          <Box sx={{ border: 'solid', width: '15rem', height: '15rem' }}></Box>
+          <FormLabel>Image</FormLabel>
+          <Box
+            sx={{
+              border: 'solid',
+              width: '15rem',
+              height: '15rem',
+            }}
+          ></Box>
           <Button>upload an image</Button>
           <br />
           <FormLabel>Description</FormLabel>
-          <TextField placeholder='Write the description'></TextField>
+          <TextField
+            required
+            size='medium'
+            placeholder='Write the description'
+            sx={{ display: 'block' }}
+          ></TextField>
+          <FormControl sx={{ width: '15rem' }}>
+            <InputLabel id='demo-simple-select-label'>
+              Categodfdfries
+            </InputLabel>
+            <Select
+            // labelId='demo-simple-select-label'
+            // id='demo-simple-select'
+            // value={category}
+            // label='Cagetory'
+            // onChange={handleChange}
+            >
+              <MenuItem value={10}>English</MenuItem>
+              <MenuItem value={20}>Math</MenuItem>
+              <MenuItem value={30}>Korean</MenuItem>
+            </Select>
+          </FormControl>
 
           <Button>Submit</Button>
-
-          <FormLabel>Categories</FormLabel>
         </form>
       </Container>
     </div>
