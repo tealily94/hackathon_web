99de4da58430c32a9b9fcb200f111acf104c2b6f
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-11-19 13:47:15 -0700

    index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import lesson from './lesson';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 export default function Home() {
-  return <div>hello</div>;
+  <div style={{ height: "100vh" }}>
+    <Stack
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: "100vh" }}
+    >
+      <Typography variant="h1" component="div" gutterBottom>
+        What do you want to Upload?
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Button variant="outlined">Quiz</Button>
+        <Button variant="outlined">Lesson</Button>
+      </Stack>
+    </Stack>
+  </div>;
 }
