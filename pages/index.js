cea577eda0e949516ab516dbde3d1a211027496c
Sooryun Kim <ksr9405@gmail.com>
2022-11-19 19:00:22 -0700

    Index

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import Link from "next/link";
-import TextField from "@mui/material/TextField";
-import { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/router";
-import main from "./main";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Link from 'next/link';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import main from './main';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 export default function Home() {
-  const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const baseUrl = "https://hackathon-backend.azurewebsites.net/createLesson";
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const baseUrl = 'https://hackathon-backend.azurewebsites.net/createLesson';
   const [lesson, setLesson] = useState(0);
   const router = useRouter();
   const handleSubmit = (e) => {
@@ -32,31 +33,93 @@ export default function Home() {
         console.log(e);
       });
   };
+
+  const handleChange = function (e) {
+    setCategory(e.target.value);
+    console.log(category);
+  };
   return (
     <div>
       <Stack spacing={2}>
-        <Typography variant="h1">Welcome to the Infinite Classroom</Typography>
-        <form>
+        <Typography variant='h2' sx={{ textAlign: 'center', marginY: '1rem' }}>
+          Welcome to the Infinite Classroom
+        </Typography>
+        <form
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '5rem',
+          }}
+        >
           <div>
-            <TextField
-              id="outlined-basic"
-              label="Title"
-              variant="outlined"
-              onChange={(e) => {
-                setTitle(e.target.value);
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '1rem',
               }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Category"
-              variant="outlined"
-              onChange={(e) => {
-                setCategory(e.target.value);
+            >
+              <TextField
+                sx={{ display: 'inline-block', marginX: '1rem' }}
+                id='outlined-basic'
+                label='Title'
+                variant='outlined'
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '2rem',
               }}
-            />
-            <Button type="submit" onClick={handleSubmit}>
-              Create a Lesson
-            </Button>
+            >
+              {/* Category Select */}
+              <FormControl
+                style={{
+                  width: '100%',
+                  textAlign: 'center',
+                  marginY: '1rem',
+                  marginX: 'auto',
+                }}
+              >
+                <InputLabel id='demo-simple-select-label'>Category</InputLabel>
+                <Select
+                  value={category}
+                  label='Cagetory'
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}
+                >
+                  <MenuItem value='English'>English</MenuItem>
+                  <MenuItem value='Math'>Math</MenuItem>
+                  <MenuItem value='Korean'>Korean</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Button
+                variant='outlined'
+                type='submit'
+                onClick={handleSubmit}
+                sx={{ display: 'block' }}
+              >
+                Create a Lesson
+              </Button>
+            </div>
           </div>
         </form>
       </Stack>
