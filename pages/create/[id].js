e46eadf07aae87184185611104de50c5ad429c45
Don Oh <okdonguk0@gmail.com>
2022-11-19 18:08:28 -0700

    complete update side web

--- pages/create/[id].js
+++ pages/create/[id].js
@@ -5,40 +5,42 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 function create() {
-  const router = useRouter();
-  console.log(router.query.id);
-  return (
-    <div>
-      <div style={{ height: "100vh" }}>
-        <Stack
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          style={{ height: "100vh" }}
-        >
-          <Typography variant="h1" component="div" gutterBottom>
-            What do you want to Upload?
-          </Typography>
-          <Stack direction="row" spacing={2}>
-            <Button
-              variant="outlined"
-              style={{ padding: "10em 15em", borderRadius: "15px" }}
-            >
-              Quiz
-            </Button>
-            <Link href={`lesson/${router.query.id}`}>
-              <Button
-                variant="outlined"
-                style={{ padding: "10em 15em", borderRadius: "15px" }}
-              >
-                Lesson
-              </Button>
-            </Link>
-          </Stack>
-        </Stack>
-      </div>
-    </div>
-  );
+    const router = useRouter();
+    console.log(router.query.id);
+    return (
+        <div>
+            <div style={{ height: "100vh" }}>
+                <Stack
+                    spacing={2}
+                    justifyContent="center"
+                    alignItems="center"
+                    style={{ height: "100vh" }}
+                >
+                    <Typography variant="h1" component="div" gutterBottom>
+                        What do you want to Upload?
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                        <Link href={`quiz/${router.query.id}`}>
+                            <Button
+                                variant="outlined"
+                                style={{ padding: "10em 15em", borderRadius: "15px" }}
+                            >
+                                Quiz
+                            </Button>
+                        </Link>
+                        <Link href={`lesson/${router.query.id}`}>
+                            <Button
+                                variant="outlined"
+                                style={{ padding: "10em 15em", borderRadius: "15px" }}
+                            >
+                                Lesson
+                            </Button>
+                        </Link>
+                    </Stack>
+                </Stack>
+            </div>
+        </div>
+    );
 }
 
 export default create;
