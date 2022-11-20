db8b4a0fba8676c2125d3ba7690860c42b7f5e2f
Sooryun Kim <ksr9405@gmail.com>
2022-11-20 09:33:00 -0700

    Index styling

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import Link from "next/link";
-import TextField from "@mui/material/TextField";
-import { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Link from 'next/link';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 export default function Home() {
-  const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const baseUrl = "https://hackathon-backend.azurewebsites.net/createLesson";
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const baseUrl = 'https://hackathon-backend.azurewebsites.net/createLesson';
   const [lesson, setLesson] = useState(0);
   const router = useRouter();
   const handleSubmit = (e) => {
@@ -40,32 +40,35 @@ export default function Home() {
   return (
     <div>
       <Stack spacing={2}>
-        <Typography variant="h2" sx={{ textAlign: "center", marginY: "1rem" }}>
+        {/* Home Page Title */}
+        <Typography variant='h2' sx={{ textAlign: 'center', marginY: '1rem' }}>
           Welcome to the Infinite Classroom
         </Typography>
+        {/* Create A Lesson Form */}
         <form
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "5rem",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '5rem',
           }}
         >
           <div>
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "1rem",
-                width: "100%",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '1rem',
+                width: '100%',
               }}
             >
+              {/* Title */}
               <TextField
-                sx={{ display: "inline-block", marginX: "1rem", width: "100%" }}
-                id="outlined-basic"
-                label="Title"
-                variant="outlined"
+                sx={{ display: 'inline-block', marginX: '1rem', width: '100%' }}
+                id='outlined-basic'
+                label='Lesson Title'
+                variant='outlined'
                 onChange={(e) => {
                   setTitle(e.target.value);
                 }}
@@ -74,48 +77,49 @@ export default function Home() {
 
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginBottom: "2rem",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '1.5rem',
               }}
             >
               {/* Category Select */}
               <FormControl
                 style={{
-                  width: "100%",
-                  textAlign: "center",
-                  marginY: "1rem",
-                  marginX: "auto",
+                  width: '86%',
+                  textAlign: 'center',
+                  marginY: '1rem',
+                  marginX: 'auto',
                 }}
               >
-                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <InputLabel id='demo-simple-select-label'>Category</InputLabel>
                 <Select
                   value={category}
-                  label="Cagetory"
+                  label='Cagetory'
                   onChange={(e) => {
                     setCategory(e.target.value);
                   }}
                 >
-                  <MenuItem value="English">English</MenuItem>
-                  <MenuItem value="Math">Math</MenuItem>
-                  <MenuItem value="Korean">Korean</MenuItem>
+                  <MenuItem value='English'>English</MenuItem>
+                  <MenuItem value='Math'>Math</MenuItem>
+                  <MenuItem value='Korean'>Korean</MenuItem>
                 </Select>
               </FormControl>
             </div>
 
+            {/* Create Button */}
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <Button
-                variant="outlined"
-                type="submit"
+                variant='outlined'
+                type='submit'
                 onClick={handleSubmit}
-                sx={{ display: "block" }}
+                sx={{ display: 'block' }}
               >
                 Create a Lesson
               </Button>
